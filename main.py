82738ab5f13f5877f12f2ca6d4bd04bd57ba0b30
CyberFlameGO <cyberflameu@gmail.com>
2021-03-24 13:13:16 +1300

    thing

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ card_kv_store = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 1, 10: 2, 11
 
 card_list_values = list(card_kv_store.values())
 random.shuffle(card_list_values)
-print("1 * * * *\n"
-      "2 * * * *\n"
-      "3 * * * *\n"
-      "4 * * * *\n"
-      "\u2588 A B C D")
+for val in range(len(card_kv_store)):
+    card_kv_store[val] = card_list_values[val-1]
+print(card_kv_store)
+print("A * * * *\n"
+      "B * * * *\n"
+      "C * * * *\n"
+      "D * * * *\n"
+      "\u2588", 1, 2, 3, 4)
