6b417724d6a8ef6acc2aa9e03b43d73b3a9d2cf2
cyberflame <cyberflameu@gmail.com>
2021-03-25 18:48:03 +1300

    stuf
    Signed-off-by: cyberflame <cyberflameu@gmail.com>

--- main.py
+++ main.py
@@ -5,15 +5,14 @@ import random
 card_kv_store = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 1, 10: 2, 11: 3, 12: 4, 13: 5, 14: 6, 15: 7,
                  16: 8}
 
-valid_rows = ["a","b","c","d"]
-valid_columns = [1,2,3,4]
+valid_rows = ["a", "b", "c", "d"]
+valid_columns = [1, 2, 3, 4]
 
 card_list_values = list(card_kv_store.values())
 random.shuffle(card_list_values)
 
 for val in range(len(card_kv_store)):
     card_kv_store[val] = card_list_values[val - 1]
-
 
 print("A * * * *\n"
       "B * * * *\n"
@@ -22,12 +21,9 @@ print("A * * * *\n"
       "\u2588",
       1, 2, 3, 4)
 
-
 row = input("Choose a line.").strip().lower()
 column = int(input("Choose a column.").strip())
-#if row in valid_rows and column in valid_columns:
-
-
+# if row in valid_rows and column in valid_columns:
 
 
 print("A {:.fdict.get[1]} {} {} {}\n"
