1a9a8cd20df529d66cc19e79d53d73e97a31fab3
cyberflame <cyberflameu@gmail.com>
2021-03-26 17:47:35 +1300

    add some functionality
    Signed-off-by: CyberFlame <cyberflameu@gmail.com>
    Signed-off-by: cyberflame <cyberflameu@gmail.com>

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ plot_number_translation = {"a1": 1, "a2": 2, "a3": 3, "a4": 4, "b1": 5, "b2": 6,
                            "c3": 11, "c4": 12, "d1": 13, "d2": 14, "d3": 15, "d4": 16}
 valid_rows = ["a", "b", "c", "d"]
 valid_columns = ["1", "2", "3", "4"]
-
+wins = 0
 playing = True
 
 print("Welcome to a round of Memory Game!\nThis is what the board looks like!\n\n"
@@ -41,11 +41,14 @@ while playing:
     #        card_kv_store[val] = card_list_values[val - 1]
     #        print(card_kv_store[val])
 
-    print("A * * * *\n"
-          "B * * * *\n"
-          "C * * * *\n"
-          "D * * * *\n"
-          "\u2588",
+    print("A {} {} {} {}\n"
+          "B {} {} {} {}\n"
+          "C {} {} {} {}\n"
+          "D {} {} {} {}\n"
+          "\u2588".format(card_kv_store.get(1), card_kv_store.get(2), card_kv_store.get(3), card_kv_store.get(4),
+                          card_kv_store.get(5), card_kv_store.get(6), card_kv_store.get(7), card_kv_store.get(8),
+                          card_kv_store.get(9), card_kv_store.get(10), card_kv_store.get(11), card_kv_store.get(12),
+                          card_kv_store.get(13), card_kv_store.get(14), card_kv_store.get(15), card_kv_store.get(16)),
           1, 2, 3, 4)
     card_kv_store = card_kv_store.fromkeys(card_kv_store, "*")
     # This is just a prototype. Maybe for the actual thing, I'll make it just one question per position
@@ -79,14 +82,25 @@ while playing:
             f"is {match2} though!"
             "\n\nWe'll be hiding these values in 3 seconds, so memorize up!")
         clear_py_console(3, 1000)
-    print("A {} {} {} {}\n"
-          "B {} {} {} {}\n"
-          "C {} {} {} {}\n"
-          "D {} {} {} {}\n"
-          "\u2588".format(card_kv_store.get(1), card_kv_store.get(2), card_kv_store.get(3), card_kv_store.get(4),
-                          card_kv_store.get(5), card_kv_store.get(6), card_kv_store.get(7), card_kv_store.get(8),
-                          card_kv_store.get(9), card_kv_store.get(10), card_kv_store.get(11), card_kv_store.get(12),
-                          card_kv_store.get(13), card_kv_store.get(14), card_kv_store.get(15), card_kv_store.get(16)),
-          1, 2, 3, 4)
-    if "*" not in card_kv_store:
-        print("Well done! Game completed, would you like to play another round?")
+    if "*" not in card_kv_store.values():
+        round_in_progress = False
+        # TODO: potentially add a stopwatch
+        wins += 1
+        print("Looks like you paired up all the numbers!\n"
+              "A {} {} {} {}\n"
+              "B {} {} {} {}\n"
+              "C {} {} {} {}\n"
+              "D {} {} {} {}\n"
+              "\u2588".format(card_kv_store.get(1), card_kv_store.get(2), card_kv_store.get(3), card_kv_store.get(4),
+                              card_kv_store.get(5), card_kv_store.get(6), card_kv_store.get(7), card_kv_store.get(8),
+                              card_kv_store.get(9), card_kv_store.get(10), card_kv_store.get(11), card_kv_store.get(12),
+                              card_kv_store.get(13), card_kv_store.get(14), card_kv_store.get(15),
+                              card_kv_store.get(16)),
+              1, 2, 3, 4)
+        round_end = input(
+            "Well done! Game completed, would you like to play another round?\nType 'y' to play another round, "
+            "or anything else to finish this session.\nInput: ").lower().strip()
+        if round_end == "y":
+            print("dope!")
+        else:
+            print("Your wins this session:", wins)
