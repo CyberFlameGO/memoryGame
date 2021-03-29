f2476f086a8ad813cfbe203d1bb8310525a9091f
cyberflame <cyberflameu@gmail.com>
2021-03-29 20:23:18 +1300

    AYYY I FKIN FIXED IT BBGIRL
    Signed-off-by: cyberflame <cyberflameu@gmail.com>

--- main.py
+++ main.py
@@ -103,11 +103,11 @@ while playing:
             if (row2 in valid_rows and 1 <= column2 <= 4):
                 if row2 != row1 and column2 != column1:
                     column2 = str(column2)
-                    pos2: str = row2 + column2  # adds the variables into one "word" (the colon after the var-name is for
-                    # annotation, which was a suggestion from my IDE)
+                    pos2: str = row2 + column2  # adds the variables into one "word" (the colon after the var-name is
+                    # for annotation, which was a suggestion from my IDE)
                     numeric_pos2 = plot_number_translation.get(pos2)  # translates the plotted point into a number
-                    int(numeric_pos2)  # makes sure it is of integer datatype (for the final script I'll check if this is
-                    # redundant)
+                    int(numeric_pos2)  # makes sure it is of integer datatype (for the final script I'll check if
+                    # this is redundant)
                     match2 = card_list_values[numeric_pos2 + 1]
                     # plotting = False
                     plotting = False
